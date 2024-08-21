d83660fe7168323fac81246540c32913a59ab705
Shenr0n <xenonsid007@gmail.com>
2024-08-21 03:31:06 -0400

    Updated Readme, added error handling, added inputs and testing outputs

--- README.md
+++ README.md
@@ -1,13 +1,38 @@
 ## Assumptions and Notes
-- The flow logs entries only supports the version 2 default log format as described in the following [Amazon VPC documentation](https://docs.aws.amazon.com/vpc/latest/userguide/flow-log-records.html).
-- All input files should be in the same folder as the program "flow_log_count.py" and "protocol-numbers-1.csv"
-- The flow logs and lookup table are in files called "flow_log.txt" and "lookup_table.csv" respectively.
+- The flow logs entries only supports the version 2 default log format as described in the following [Amazon VPC documentation](https://docs.aws.amazon.com/vpc/latest/userguide/flow-log-records.html). It is a text file and each log entry has 14 fields. The lookup table is defined as a csv file, and it has 3 columns, dstport,protocol,tag.
 - "protocol-numbers-1.csv" contains the [Assigned Internet Protocol Numbers from IANA](https://www.iana.org/assignments/protocol-numbers/protocol-numbers.xhtml), which I used, to get the protocol names from protocol numbers in flow logs. **This file is required for the program's operations.**
-- The generated output is split into two files "tags_count.txt" and "port_protocol_count.txt" containing the Tag counts and Port/Protocol Combination counts respectively. The input files can be very large with thousands of mappings, so I split up the output into two files for ease of parsing. I skipped the "Tag Counts: " and "Port/Protocol Combination Counts: " in the first lines of these output files.
+- The generated output is split into two files "tags_count.txt" and "port_protocol_count.txt" containing the Tag counts and Port/Protocol Combination counts respectively. The input files can be very large with thousands of mappings, so I split up the output into two files for ease of parsing. I skipped the "Tag Counts: " and "Port/Protocol Combination Counts: " strings in the first lines of these output files.
+---
+### Testing
+- Testing was successful with a flow log file above 10 MB and lookup file with more than 14000 mappings.
+- All other requirements from the instruction email, have been tested and satisfied as well.
+- The sample input and output files from testing, have been provided in this repository.
+- The program works without a Buffered Reader as the files are small enough.
+---
+### Techstack
 
-
-## Techstack
-
-- [Python 3.9.1]([https://docs.python.org/3/library/csv.html])
+- [Python 3.9.1](https://docs.python.org)
 - Visual Studio Code
+---
+### Instructions to Compile/Run
+- I used Python 3.9.1 for this assessment.
+- All input files should be in the same folder as the program "flow_log_count.py" and "protocol-numbers-1.csv" **Files required.**
+- The flow logs and lookup table inputs should be in files called "flow_log.txt" and "lookup_table.csv" respectively. Renaming of your input files might be required, if necessary.
+- Two files are generated as outputs "tags_count.txt" and "port_protocol_count.txt".
 
+##### After all input and program files are in the same folder, the following can be input in terminal to generate output files:
+
+```
+python3 flow_log_count.py
+```
+or
+```
+python flow_log_count.py
+```
+
+### Other References
+https://docs.python.org/3/tutorial/errors.html
+
+https://docs.python.org/3/library/string.html
+
+https://docs.python.org/3/library/collections.html#collections.defaultdict
--- flow_log_count.py
+++ flow_log_count.py
@@ -1,28 +1,41 @@
 import csv
 from collections import defaultdict
 
-
 # Function to extract information from lookup input file and the protocol numbers with keywords to dictionaries
 def csv_to_dict(protocol_info, lookup_table):
-    protocol_numbers = {}
-    lookup = {}
+    protocol_numbers = defaultdict(str)
+    lookup = defaultdict(str)
 
     # Extract protocol info
-    with open(protocol_info, 'r', newline='') as csvfile:
-        protocol_reader = csv.DictReader(csvfile, delimiter=',')
-        #next(protocol_reader)
-        for protocol_row in protocol_reader:
-            p_key = protocol_row['Decimal']
-            # Removing the " (deprecated)" string from keyword
-            p_value = protocol_row['Keyword'].lower().split(' (')[0]
-            protocol_numbers[p_key] = p_value
+    try:
+        with open(protocol_info, 'r', newline='') as csvfile:
+            protocol_reader = csv.DictReader(csvfile, delimiter=',')
+            #next(protocol_reader)
+            for protocol_row in protocol_reader:
+                p_key = protocol_row['Decimal']
+                # Removing the " (deprecated)" string from keyword
+                p_value = protocol_row['Keyword'].lower().split(' (')[0]
+                protocol_numbers[p_key] = p_value
+    except FileNotFoundError:
+        print(f"File {protocol_info} not found")
+        return None, None
+    except Exception as e:
+        print(f"Error {e}")
+        return None, None
 
     # Extract lookup table info
-    with open(lookup_table, 'r', newline='') as csvfile:
-        lookup_reader = csv.DictReader(csvfile, delimiter=',')
-        for lookup_row in lookup_reader:
-            l_key = (lookup_row['dstport'],lookup_row['protocol'].lower())
-            lookup[l_key] = lookup_row['tag']
+    try:
+        with open(lookup_table, 'r', newline='') as csvfile:
+            lookup_reader = csv.DictReader(csvfile, delimiter=',')
+            for lookup_row in lookup_reader:
+                l_key = (lookup_row['dstport'],lookup_row['protocol'].lower())
+                lookup[l_key] = lookup_row['tag']
+    except FileNotFoundError:
+        print(f"File {lookup_table} not found")
+        return None, None
+    except Exception as e:
+        print(f"Error {e}")
+        return None, None
 
     return protocol_numbers, lookup
 
@@ -34,22 +47,29 @@ def get_count(flow_log, protocol_numbers, lookup_table):
     tags_count_dict = defaultdict(int)
     port_protocol_count_dict = defaultdict(int)
 
-    with open(flow_log, 'r', newline='') as logfile:
-        for each_log in logfile:
-            log_info = each_log.split()
-            # Get dstport, protocol number from flow logs and then protocol name from protocol dict
-            dstport, protocol = log_info[6], protocol_numbers[log_info[7]]
+    try:
+        with open(flow_log, 'r', newline='') as logfile:
+            for each_log in logfile:
+                log_info = each_log.strip().split()
+                # Get dstport, protocol number from flow logs and then protocol name from protocol dict
+                dstport, protocol = log_info[6], protocol_numbers[log_info[7]]
 
-            # Check if (dstport,protocol) is in lookup dict, increment it's count or else mark it as untagged
-            lookup_key = (dstport, protocol)
+                # Check if (dstport,protocol) is in lookup dict, increment it's count or else mark it as untagged
+                lookup_key = (dstport, protocol)
 
-            if lookup_key in lookup_table:
-                tags_count_dict[lookup_table[lookup_key]] += 1
-            else:
-                untagged_count += 1
-            
-            # Increment count value of (dstport,protocol) in port_protocol_op dict
-            port_protocol_count_dict[lookup_key] += 1
+                if lookup_key in lookup_table:
+                    tags_count_dict[lookup_table[lookup_key]] += 1
+                else:
+                    untagged_count += 1
+                
+                # Increment count value of (dstport,protocol) in port_protocol_op dict
+                port_protocol_count_dict[lookup_key] += 1
+    except FileNotFoundError:
+        print(f"File {flow_log} not found")
+        return None, None
+    except Exception as e:
+        print(f"Error {e}")
+        return None, None
 
     tags_count_dict['Untagged'] = untagged_count
     return tags_count_dict, port_protocol_count_dict
@@ -58,19 +78,25 @@ def get_count(flow_log, protocol_numbers, lookup_table):
 # Create a new output file for tags and port/protocol counts each, and write to it
 def generate_output(tags_count_dict, port_protocol_count_dict, tags_count, port_protocol_count):
     
-        # Write Tag Counts output file
+    # Write Tag Counts output file
+    try:
         with open(tags_count, 'w', newline='') as tags_count_file:
             tags_count_file.write("Tag,Count\n")
             for tag, count in tags_count_dict.items():
-                tags_count_file.write(tag+','+str(count)+'\n')
-        
-        # Write Port Protocol Combination Counts output file
+                tags_count_file.write(f"{tag},{count}\n")
+    except Exception as e:
+        print(f"Error {e}")
+
+
+    # Write Port Protocol Combination Counts output file
+    try:
         with open(port_protocol_count, 'w', newline='') as port_protocol_count_file:
             port_protocol_count_file.write("Port,Protocol,Count\n")
             for (port, protocol), count in port_protocol_count_dict.items():
-                port_protocol_count_file.write(port+','+protocol+','+str(count)+'\n')
-
-
+                #port_protocol_count_file.write(port+','+protocol+','+str(count)+'\n')
+                port_protocol_count_file.write(f"{port},{protocol},{count}\n")
+    except Exception as e:
+        print(f"Error {e}")
 
 def main():
 
